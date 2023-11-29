ffcbedc3c091a033a2496080df64006f764f7d8e
rodrigues lima <limamateus@LIVE.COM>
2023-11-28 21:08:27 -0300

    Api com GIN e GORM

--- controller/alunosController.go
+++ controller/alunosController.go
@@ -9,8 +9,9 @@ import (
 )
 
 func ListaDeAlunos(c *gin.Context) {
-
-	c.JSON(200, gin.H{})
+	var alunos []models.Aluno // Aqui estou criando uma variavel que irá representar uma lista de alunos
+	database.DB.Find(&alunos) // depois vou no banco e passo os dados para variavel
+	c.JSON(200, alunos)       // retorno como json status 200 e a lista.
 }
 
 func Saudacao(c *gin.Context) {
@@ -32,4 +33,75 @@ func NovoAluno(c *gin.Context) {
 	database.DB.Create(&aluno) // Aqui estou salvando os dados do aluno no banco
 
 	c.JSON(http.StatusOK, aluno)
+}
+
+func AlunoPorId(c *gin.Context) {
+	var aluno models.Aluno // Aqui eu crio uma variavel local que irá representar o aluno
+
+	id := c.Params.ByName("id") // Aqui estou pegando o valor do id passado na rota
+	// a ordem é a variavel depois o id
+	database.DB.First(&aluno, id) // aqui estou armanzendo o retorno de acordo com a consulta na variavel local
+	if aluno.ID == 0 {            // Aqui eu valido se o Id é igual a 0 e retorno bad request com mesagem de erro.
+		c.JSON(http.StatusNotFound, gin.H{
+			"Erro": "Aluno não econtrando"})
+		return
+
+	}
+
+	c.JSON(http.StatusOK, aluno) // Caso de tudo certo retorno o aluno e status ok
+}
+
+func DeletarAlunoPorId(c *gin.Context) {
+	id := c.Params.ByName("id") // Aqui estou pegando o Id da rota
+
+	var aluno models.Aluno // Criando uma variavel local
+
+	database.DB.First(&aluno, id) // realizo a consulta no banco
+
+	if aluno.ID == 0 { // Se não encontra do bad request e informo que não foi encontrado.
+		c.JSON(http.StatusNotFound, gin.H{"Erro": "Aluno não encontrado"})
+		return
+	}
+
+	database.DB.Delete(&aluno, id) // Deleto do banco
+
+	c.JSON(http.StatusNoContent, nil) // retorno status de 201 no contant
+
+}
+
+func EditarAluno(c *gin.Context) {
+	id := c.Params.ByName("id") // Aqui estou pegando o Id da rota
+
+	var aluno models.Aluno // Criando uma variavel local
+
+	database.DB.First(&aluno, id) // realizo a consulta no banco
+
+	if aluno.ID == 0 { // Se não encontra do bad request e informo que não foi encontrado.
+		c.JSON(http.StatusNotFound, gin.H{"Erro": "Aluno não encontrado"})
+		return
+	}
+
+	if err := c.ShouldBindJSON(&aluno); err != nil { // Aqui estou validando objeto da requisição
+		c.JSON(http.StatusNotFound, gin.H{"Erro": err.Error()})
+		return
+	}
+
+	database.DB.Model(&aluno).UpdateColumns(aluno) // Deleto do banco
+
+	c.JSON(http.StatusNoContent, nil) // retorno status de 201 no contant
+}
+
+func BuscarAlunoPorCpf(c *gin.Context) {
+	cpf := c.Param("cpf") // Aqui eu pego o valor do cpf
+
+	var aluno models.Aluno // estancio uma variavel local que será usada como referencia
+
+	database.DB.Where(&models.Aluno{CPF: cpf}).First(&aluno) // Realizo uma consulta do banco de acordo com cpf e amarzeno o resultado em aluno
+
+	if aluno.ID == 0 { // Se o aluno não foi encontra do bad request e informo que não foi encontrado.
+		c.JSON(http.StatusNotFound, gin.H{"Erro": "Aluno não encontrado"})
+		return
+	}
+
+	c.JSON(http.StatusOK, aluno) // retorno status de Ok e o aluno no contant
 }
--- routes/routes.go
+++ routes/routes.go
@@ -11,5 +11,9 @@ func HendlerRequest() {
 	r.GET("/", controller.ListaDeAlunos)
 	r.GET("/:nome", controller.Saudacao)
 	r.POST("/alunos/novo", controller.NovoAluno)
+	r.GET("/alunos/:id", controller.AlunoPorId)
+	r.DELETE("/alunos/:id", controller.DeletarAlunoPorId)
+	r.PATCH("/alunos/:id", controller.EditarAluno)
+	r.GET("/alunos/cpf/:cpf", controller.BuscarAlunoPorCpf)
 	r.Run()
 }
