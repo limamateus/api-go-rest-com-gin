96482e2d83ad4130c82a69ac0a5c5b724c8146c0
rodrigues lima <limamateus@LIVE.COM>
2023-11-29 21:26:50 -0300

    Melhorando a validação de alunos.

--- models/aluno.go
+++ models/aluno.go
@@ -1,20 +1,39 @@
 package models
 
 import (
-	"gopkg.in/validator.v2"
+	"errors"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
 	"gorm.io/gorm"
 )
 
 type Aluno struct {
 	gorm.Model
-	Nome string `json:"nome" validate:"nonzero" `
-	CPF  string `json:"cpf" validate:"nonzero, len=11, regexp=^[0-9]*$"`
+	Nome string `json:"nome" validate:"required"`
+	CPF  string `json:"cpf" validate:"required,len=11"`
 }
 
 func ValidacaoDoAluno(aluno *Aluno) error {
-	if err := validator.Validate(aluno); err != nil { // Estou usando a bilbiote validator para validar as propriedade e retornar ume erro com base nas proriedades.
-		return err
+	validate := validator.New()
+	if err := validate.Struct(aluno); err != nil {
+		if _, ok := err.(*validator.InvalidValidationError); ok {
+			return err
+		}
+
+		var fieldErrors []string
+		for _, err := range err.(validator.ValidationErrors) {
+			switch err.Tag() {
+			case "required":
+				fieldErrors = append(fieldErrors, "O campo '"+err.Field()+"' é obrigatório")
+			case "len":
+				fieldErrors = append(fieldErrors, "O campo '"+err.Field()+"' deve ter 11 dígitos")
+			default:
+				fieldErrors = append(fieldErrors, "Erro de validação no campo '"+err.Field()+"'")
+			}
+		}
+
+		return errors.New("Erros de validação: " + strings.Join(fieldErrors, ", "))
 	}
 	return nil
-
 }
